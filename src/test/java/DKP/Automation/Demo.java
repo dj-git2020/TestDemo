9af12c479fd71e2cd72d4ecae7ac78d596711cf3
Dhananjay <dhananjaypandey06@gmail.com>
2024-05-22 21:39:16 +0530

    first commit

---
+++ src/test/java/DKP/Automation/Demo.java
@@ -0,0 +1,19 @@
+package DKP.Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(1000);
+		driver.close();
+	}
+
+}
